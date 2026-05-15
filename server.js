367501b50ae72be2b24245ca8fb6c9c7e689396a
lucyskyes35-beep <lucyskyes35@gmail.com>
2026-05-15 07:13:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   'http://127.0.0.1:5000',
-  'https://bellcaen.netlify.app',
+  'https://bellupdate.netlify.app',
   'https://wwbellbusiness.vercel.app',
   'https://thebellca.netlify.app',
   'https://bellnewbill.netlify.app',
